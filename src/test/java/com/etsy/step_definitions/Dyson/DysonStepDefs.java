a785a8df7d0adf08a50d3d4545863c80fe9e3f9b
Mehmet <mehmetfatihacar@outlook.com>
2020-08-18 19:48:35 +0100

    Updating changes

--- src/test/java/com/etsy/step_definitions/Dyson/DysonStepDefs.java
+++ src/test/java/com/etsy/step_definitions/Dyson/DysonStepDefs.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 
 public class DysonStepDefs {
 
-    private String URL="https://www.dyson.co.uk/sticks/dyson-cyclone-v10-shop-all.html";
+    private String URL = "https://www.dyson.co.uk/sticks/dyson-cyclone-v10-shop-all.html";
 
     @When("I am on the v10 page")
     public void i_am_on_the_v10_page() throws InterruptedException {
@@ -20,7 +20,7 @@ public class DysonStepDefs {
 
     @Then("the (.*) should be displayed")
     public void the_should_be_displayed(String message) {
-        WebElement actualMessage = Driver.get().findElement(By.className("trade-up-item__stock-message"));
-        Assert.assertEquals(message,actualMessage.getText());
+        WebElement actualMessage = Driver.get().findElement(By.xpath("(//li[@data-product-id='226372-01'])[2]//div[@class='trade-up-item__stock-message']"));
+        Assert.assertEquals(message, actualMessage.getText());
     }
 }
