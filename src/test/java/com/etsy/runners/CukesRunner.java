6e6e7b01652ee462dc313cf17a0614539237f544
Mehmet <mehmetfatihacar@outlook.com>
2020-10-17 10:16:46 +0100

    Updating changes

--- src/test/java/com/etsy/runners/CukesRunner.java
+++ src/test/java/com/etsy/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/etsy/step_definitions",
         dryRun = false,
-        tags ="@dyson"
+        tags ="@test"
 )
 public class CukesRunner {
 }
